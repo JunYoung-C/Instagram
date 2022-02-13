a391a405cc919ea8718c34d80ee2446fac3b4460
JunYoung-C <doforme524@gmail.com>
2022-02-13 21:00:58 +0900

    Chore: 스프링 스타터로 파일 생성

---
+++ instragram/src/main/java/toyproject/instragram/InstragramApplication.java
@@ -0,0 +1,13 @@
+package toyproject.instragram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstragramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstragramApplication.class, args);
+	}
+
+}
---
+++ instragram/src/test/java/toyproject/instragram/InstragramApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.instragram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstragramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
