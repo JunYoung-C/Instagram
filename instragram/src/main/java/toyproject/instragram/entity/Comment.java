ac6837f8f8758bfa467745b86418860927026f84
JunYoung-C <doforme524@gmail.com>
2022-03-21 15:10:55 +0900

    feat: Comment 엔티티 구현 (#14)

---
+++ instragram/src/main/java/toyproject/instragram/entity/Comment.java
@@ -0,0 +1,28 @@
+package toyproject.instragram.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+    private String text;
+
+    @OneToMany(mappedBy = "comment")
+    List<Reply> replies = new ArrayList<>();
+}
