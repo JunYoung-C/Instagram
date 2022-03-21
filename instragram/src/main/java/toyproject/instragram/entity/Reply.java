c9ca155ee2f1a2d5fc69c7368912ae6fd7ad2d70
JunYoung-C <doforme524@gmail.com>
2022-03-21 15:10:42 +0900

    feat: Reply 엔티티 구현 (#14)

---
+++ instragram/src/main/java/toyproject/instragram/entity/Reply.java
@@ -0,0 +1,23 @@
+package toyproject.instragram.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "reply_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+    private String text;
+}
