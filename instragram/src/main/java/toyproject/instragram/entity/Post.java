7523444ded9224abc9daa6b09b2d2e6ae3e6f947
JunYoung-C <doforme524@gmail.com>
2022-03-21 15:11:35 +0900

    feat: Post 엔티티 구현 (#14)

---
+++ instragram/src/main/java/toyproject/instragram/entity/Post.java
@@ -0,0 +1,29 @@
+package toyproject.instragram.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String photoPath;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+}
