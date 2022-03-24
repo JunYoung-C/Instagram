e5c7ad3f03834b000819a38fea6b610858ebf29e
JunYoung-C <doforme524@gmail.com>
2022-03-25 00:05:54 +0900

    feat: 댓글에 달린 총 답글의 수를 조회하는 기능 추가 (#18)

---
+++ instragram/src/main/java/toyproject/instragram/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package toyproject.instragram.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import toyproject.instragram.entity.Reply;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    Long countRepliesByCommentId(Long commentId);
+}
