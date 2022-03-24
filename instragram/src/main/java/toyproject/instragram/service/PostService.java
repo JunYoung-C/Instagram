e8cd1b668a737861b0de86bb02ef2a46e4ae7229
JunYoung-C <doforme524@gmail.com>
2022-03-24 16:02:18 +0900

    refactor: PostRepository 게시물 조회 메소드가 dto 조회에서 객체 조회로 변경됨에 따른 Service 코드 변경 (#16)

--- instragram/src/main/java/toyproject/instragram/service/PostService.java
+++ instragram/src/main/java/toyproject/instragram/service/PostService.java
@@ -1,14 +1,20 @@
 package toyproject.instragram.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.instragram.entity.Comment;
 import toyproject.instragram.entity.Post;
-import toyproject.instragram.repository.MemberRepository;
+import toyproject.instragram.repository.CommentRepository;
 import toyproject.instragram.repository.PostRepository;
+import toyproject.instragram.repository.dto.PostCommentDto;
 import toyproject.instragram.repository.dto.PostDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,7 +22,9 @@ import toyproject.instragram.repository.dto.PostDto;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final MemberRepository memberRepository;
+    private final CommentRepository commentRepository;
+
+    private final int MAX_COMMENT_COUNT = 3;
 
     @Transactional
     public Long addPost(Post post) {
@@ -25,8 +33,30 @@ public class PostService {
         return post.getId();
     }
 
-    public Slice<PostDto> getPosts(Pageable pageable) {
-        return postRepository.getPostsByOrderByCreatedDateDesc(pageable);
+    public Slice<PostDto> getPostDtoSlice(Pageable pageable) {
+        Slice<Post> postSlice = postRepository.getPostsByOrderByCreatedDateDesc(pageable);
+        return postSlice
+                .map(post -> new PostDto(post, getTotalComments(post), getCommentDtoLists(getComments(post))));
+    }
+
+    private Long getTotalComments(Post post) {
+        return commentRepository.countCommentsByPostId(post.getId());
+    }
+
+    private List<PostCommentDto> getCommentDtoLists(List<Comment> comments) {
+        return comments
+                .stream()
+                .map(comment -> new PostCommentDto(comment.getId(), comment.getText()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Comment> getComments(Post post) {
+        return commentRepository
+                .getCommentsByMemberIdAndPostId(
+                        post.getMember().getId(),
+                        post.getId(),
+                        PageRequest.of(0, MAX_COMMENT_COUNT))
+                .getContent();
     }
 
     @Transactional
@@ -34,3 +64,4 @@ public class PostService {
         postRepository.deleteById(postId);
     }
 }
+
