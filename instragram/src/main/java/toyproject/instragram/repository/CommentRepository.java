478892f51d32b239b1e9d1e774d210a0fe909e7e
JunYoung-C <doforme524@gmail.com>
2022-03-25 11:10:07 +0900

    feat: CommentRepository에서 댓글을 조회하는 메소드에 member 페치조인 적용

--- instragram/src/main/java/toyproject/instragram/repository/CommentRepository.java
+++ instragram/src/main/java/toyproject/instragram/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package toyproject.instragram.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.instragram.entity.Comment;
 
@@ -9,7 +10,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Long countCommentsByPostId(Long postId);
 
+    @EntityGraph(attributePaths = {"member"})
     Slice<Comment> getCommentsByMemberIdAndPostId(Long memberId, Long postId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member"})
     Slice<Comment> getCommentsByPostIdOrderByCreatedDateDesc(Long postId, Pageable pageable);
 }
