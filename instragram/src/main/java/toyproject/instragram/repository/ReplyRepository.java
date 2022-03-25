aef9b64dcaf31c209cc2879dcf962347c44f00cf
JunYoung-C <doforme524@gmail.com>
2022-03-25 09:29:01 +0900

    feat: 생성 날짜가 빠른 순으로 답글을 조회하는 기능 추가 (#18)

--- instragram/src/main/java/toyproject/instragram/repository/ReplyRepository.java
+++ instragram/src/main/java/toyproject/instragram/repository/ReplyRepository.java
@@ -1,8 +1,13 @@
 package toyproject.instragram.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.instragram.entity.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
     Long countRepliesByCommentId(Long commentId);
+
+    Slice<Reply> getRepliesByCommentIdOrderByCreatedDateDesc(Long commentId, Pageable pageable);
 }
