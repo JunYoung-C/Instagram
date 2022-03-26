b775f208680c657c64c7dd0642c993ed7af5cf38
JunYoung-C <doforme524@gmail.com>
2022-03-26 10:57:43 +0900

    feat: 미리보기 슬라이딩 기능 구현 (#10)

--- instragram/src/main/resources/static/javascript/main.js
+++ instragram/src/main/resources/static/javascript/main.js
@@ -1,8 +1,22 @@
-const imagePrevButton = document.querySelector(".image-prev-button");
-const imageNextButton = document.querySelector(".image-next-button");
-const postImagesCnt = document.querySelectorAll(".post-images__list").length;
+const postImagePrevButton = document.querySelector(".post-images__prev-button");
+const postImageNextButton = document.querySelector(".post-images__next-button");
+const postImagesCount = document.querySelectorAll(".post-images__list").length;
 const postImages = document.querySelector(".post-images");
-const slideWidth = 600;
+
+const mainBody = document.querySelector(".main-body");
+const showAddPostButton = document.querySelector(".show-add-post");
+const addPostDiv = document.querySelector("#add-post");
+const addPostCancleButtons = document.querySelectorAll(".add-post-cancel");
+const addPostFileInput = document.querySelector(".add-post__file-input");
+const addPostPreview = document.querySelector(".add-post__preview");
+const addPostText = document.querySelector(".add-post__text");
+
+const previewPrevButton = document.querySelector(".add-post__preview-prev-button");
+const previewNextButton = document.querySelector(".add-post__preview-next-button");
+const previewListCount = document.querySelectorAll(".add-post__preview-list").length;
+
+const POST_IMAGE_WIDTH = 600;
+const PREVIEW_IMAGE_WIDTH = 453.59;
 
 class SlideController {
   constructor(slideWidth, imageCount, prevButton, nextButton, images) {
@@ -18,6 +32,7 @@ class SlideController {
     const increment = (selectedButton === this.prevButton) ? -1 : 1;
   
     this.index += increment;
+    console.log(this.index);
     this.images.style.left = -(this.index * this.slideWidth) + "px";
     this.controlButtons();
   }
@@ -47,20 +62,80 @@ class SlideController {
   isVisible(imageButton) {
     imageButton.classList.contains("unvisible-button");
   }
-}
 
-const postImageSlideController = new SlideController(slideWidth, postImagesCnt, imagePrevButton, imageNextButton, postImages);
-
-imagePrevButton.addEventListener("click", (event) => {
-  postImageSlideController.slide(event.currentTarget);
-});
-
-imageNextButton.addEventListener("click", (event) => {
-  postImageSlideController.slide(event.currentTarget);
-});
-
-window.onload = function() {
-  if (postImagesCnt === 1) {
-    removeButton(imageNextButton);
+  clear(imageCount) {
+    this.index = 0;
+    this.imageCount = imageCount;
   }
 }
+
+function showPreviewFiles(event) {
+  const files = event.target.files;
+
+  Array.from(files).forEach((file) => {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function(event) {
+      const img = document.createElement("img");
+      img.src = event.target.result;
+      img.classList.add("add-post__preview-image");
+
+      const li = document.createElement("li");
+      li.classList.add("add-post__preview-list");
+
+      li.appendChild(img);
+      addPostPreview.appendChild(li);
+    });
+  
+    reader.readAsDataURL(file);
+  });
+
+  previewSlideController.clear(files.length);
+  console.log(files.length);
+}
+
+function postFormClear() {
+  addPostPreview.innerHTML = "";
+  addPostFileInput.value = "";
+  addPostText.value = "";
+}
+
+const postImageSlideController = new SlideController(POST_IMAGE_WIDTH, postImagesCount, postImagePrevButton, postImageNextButton, postImages);
+
+postImagePrevButton.addEventListener("click", (event) => {
+  postImageSlideController.slide(event.currentTarget);
+});
+
+postImageNextButton.addEventListener("click", (event) => {
+  postImageSlideController.slide(event.currentTarget);
+});
+
+addPostCancleButtons.forEach((addPostCancleBtn) => {
+  addPostCancleBtn.addEventListener("click", () => {
+    addPostDiv.style.display = "none";
+    mainBody.style.overflow = "auto";
+    postFormClear();
+  })
+});
+
+showAddPostButton.addEventListener("click", () => {
+  addPostDiv.style.display = "block";
+  mainBody.style.overflow = "hidden";
+});
+
+const previewSlideController = new SlideController(PREVIEW_IMAGE_WIDTH, previewListCount, previewPrevButton, previewNextButton, addPostPreview);
+
+previewPrevButton.addEventListener("click", (event) => {
+  previewSlideController.slide(event.currentTarget);
+});
+
+previewNextButton.addEventListener("click", (event) => {
+  previewSlideController.slide(event.currentTarget);
+});
+
+addPostFileInput.addEventListener("change", showPreviewFiles);
+
+if (postImagesCount === 1) {
+  postImageNextButton.classList.add("unvisible-button");
+}
+
