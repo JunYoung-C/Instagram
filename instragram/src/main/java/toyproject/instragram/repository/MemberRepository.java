cfe0404f3b48ed9b8fbf47bf8b85404ac197f1a7
JunYoung-C <doforme524@gmail.com>
2022-04-02 23:05:28 +0900

    test: 변경된 MemberRepository 기능 테스트

--- instragram/src/main/java/toyproject/instragram/repository/MemberRepository.java
+++ instragram/src/main/java/toyproject/instragram/repository/MemberRepository.java
@@ -3,9 +3,7 @@ package toyproject.instragram.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import toyproject.instragram.entity.Member;
-import toyproject.instragram.entity.Profile;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
--- instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
+++ instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import toyproject.instragram.AppConfig;
+import toyproject.instragram.dto.MemberProfileDto;
 import toyproject.instragram.entity.Member;
+import toyproject.instragram.entity.MemberImage;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,20 +26,29 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @BeforeEach
     void init() {
-        Profile profile1 = new Profile("junyoung", "이름1", null);
-        Member member1 = new Member(null, profile1);
+
+        MemberImage memberImage1 = new MemberImage("file1", "encodedFile1", "png");
+        Member member1 = new Member(null, "junyoung", "이름1");
+        member1.addProfileImage(memberImage1);
         memberRepository.save(member1);
 
-        Profile profile2 = new Profile("jun_young", "이름2", null);
-        Member member2 = new Member(null, profile2);
+        MemberImage memberImage2 = new MemberImage("file2", "encodedFile2", "png");
+        Member member2 = new Member(null, "jun_young", "이름2");
+        member2.addProfileImage(memberImage2);
         memberRepository.save(member2);
 
-        Profile profile3 = new Profile("chanyoung", "이름3", null);
-        Member member3 = new Member(null, profile3);
+        MemberImage memberImage3 = new MemberImage("file3", "encodedFile3", "png");
+        Member member3 = new Member(null, "chanyoung", "이름2");
+        member3.addProfileImage(memberImage3);
         memberRepository.save(member3);
 
+        em.flush();
+        em.clear();
     }
 
     @DisplayName("닉네임으로 프로필 검색 - 성공")
@@ -41,13 +56,18 @@ class MemberRepositoryTest {
     public void searchByNickname() {
         //given
         //when
-        List<Profile> findProfiles = memberRepository.searchProfiles("you");
+        List<MemberProfileDto> findProfiles = memberRepository.searchProfiles("jun");
 
         //then
         assertThat(findProfiles)
                 .extracting("nickname")
-                .containsExactly("junyoung", "jun_young", "chanyoung");
+                .containsExactly("junyoung", "jun_young");
 
+        assertThat(findProfiles)
+                .extracting("imagePath")
+                .containsExactly("encodedFile1.png", "encodedFile2.png");
+
+        assertThat(findProfiles).hasSize(2);
     }
 
     @DisplayName("닉네임으로 프로필 검색 - 결과 없음")
@@ -55,7 +75,7 @@ class MemberRepositoryTest {
     public void searchByNickname_fail() {
         //given
         //when
-        List<Profile> findProfiles = memberRepository.searchProfiles(" ");
+        List<MemberProfileDto> findProfiles = memberRepository.searchProfiles(" ");
 
         //then
         assertThat(findProfiles.size()).isEqualTo(0);
@@ -68,9 +88,9 @@ class MemberRepositoryTest {
         //given
         //when
         String nickname = "junyoung";
-        Member findMember = memberRepository.findByProfileNickname(nickname).orElse(null);
+        Member findMember = memberRepository.findByNickname(nickname).orElse(null);
 
         //then
-        assertThat(findMember.getProfile().getNickname()).isEqualTo(nickname);
+        assertThat(findMember.getNickname()).isEqualTo(nickname);
     }
 }
