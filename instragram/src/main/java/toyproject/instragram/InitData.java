88132c0206d3d4246cdc85857fd3fb79fe0e07c6
JunYoung-C <doforme524@gmail.com>
2022-04-03 23:17:16 +0900

    chore: 초기 데이터 설정

--- instragram/src/main/java/toyproject/instragram/InitData.java
+++ instragram/src/main/java/toyproject/instragram/InitData.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import toyproject.instragram.entity.Comment;
-import toyproject.instragram.entity.Member;
-import toyproject.instragram.entity.Post;
+import toyproject.instragram.entity.*;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.stream.IntStream;
 
 @Profile("local")
 @Component
@@ -31,20 +30,30 @@ public class InitPost {
 
         @PersistenceContext
         EntityManager em;
-        public void init() {
-            toyproject.instragram.entity.Profile profile1 = new toyproject.instragram.entity.Profile("nickname1", "이름1", null);
-            Member member1 = new Member(null, profile1);
-            em.persist(member1);
 
-            toyproject.instragram.entity.Profile profile2 = new toyproject.instragram.entity.Profile("nickname2", "이름2", null);
-            Member member2 = new Member(null, profile2);
+        public void init() {
+            Privacy privacy = Privacy.create()
+            Member member1 = new Member(null, "nickname1", "이름1");
+            Member member2 = new Member(null, "nickname2", "이름2");
+            em.persist(member1);
             em.persist(member2);
 
+            Post post = new Post(member1, null);
+            em.persist(post);
+
+            Comment comment = new Comment(post, member1, null);
+            em.persist(comment);
+
+            IntStream.range(0, 25)
+                    .forEach(i -> em.persist(new Reply(comment, i % 2 == 0 ? member1 : member2, String.valueOf(i))));
+
+            em.flush();
+            em.clear();
             for (int i = 0; i < 100; i++) {
                 Member member = i % 2 == 0 ? member1 : member2;
                 Post post = new Post(member, null);
                 for (int j = 0; j < i; j++) {
-                    post.addComment(new Comment(post, "안녕하세요" + i + j));
+                    post.addComment(new Comment(post, i % 2 == 0 ? member1 : member2, "안녕하세요" + i + j));
                 }
                 em.persist(post);
             }
