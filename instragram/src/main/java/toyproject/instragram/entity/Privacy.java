7a06569a5d1c19fab4a5a15054f39b0cd8ba0ec9
JunYoung-C <doforme524@gmail.com>
2022-04-03 23:21:26 +0900

    feat: Privacy에 정적 팩토리 메서드 구현 (#20)

--- instragram/src/main/java/toyproject/instragram/entity/Privacy.java
+++ instragram/src/main/java/toyproject/instragram/entity/Privacy.java
@@ -6,7 +6,6 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode
 public class Privacy {
@@ -14,4 +13,24 @@ public class Privacy {
     private String password;
     private String phoneNumber;
     private String email;
+
+    public static Privacy create(String password, String phoneNumberOrEmail) {
+        Privacy privacy = new Privacy();
+        privacy.setPassword(password);
+        privacy.setPhoneNumberOrEmail(phoneNumberOrEmail);
+
+        return privacy;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    private void setPhoneNumberOrEmail(String phoneNumberOrEmail) {
+        if (phoneNumberOrEmail.contains("@")) {
+            email = phoneNumberOrEmail;
+        } else {
+            phoneNumber = phoneNumberOrEmail;
+        }
+    }
 }
