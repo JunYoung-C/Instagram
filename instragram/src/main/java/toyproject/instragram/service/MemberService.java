9d3428bb3c07cd364d7653e9070c08d4f312163f
JunYoung-C <doforme524@gmail.com>
2022-04-03 23:18:43 +0900

    feat: MemberService signUp 메소드에서 파라미터를 dto로 받도록 변경

--- instragram/src/main/java/toyproject/instragram/service/MemberService.java
+++ instragram/src/main/java/toyproject/instragram/service/MemberService.java
@@ -3,6 +3,7 @@ package toyproject.instragram.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.instragram.entity.Privacy;
 import toyproject.instragram.repository.MemberProfileDto;
 import toyproject.instragram.entity.Member;
 import toyproject.instragram.repository.MemberRepository;
@@ -18,8 +19,11 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long signUp(Member member) {
-        validateDuplicateMember(member);
+    public Long signUp(MemberDto memberDto) {
+        validateDuplicateNickname(memberDto.getNickname());
+        Privacy privacy = Privacy.create(memberDto.getPassword(), memberDto.getPhoneNumberOrEmail());
+        Member member = new Member(privacy, memberDto.getNickname(), memberDto.getName());
+
         memberRepository.save(member);
         return member.getId();
     }
@@ -28,11 +32,11 @@ public class MemberService {
         return memberRepository.searchProfiles(nickname);
     }
 
-    private void validateDuplicateMember(Member member) {
-        Optional<Member> findMember = memberRepository.findByNickname(member.getNickname());
+    private void validateDuplicateNickname(String nickname) {
+        Optional<Member> findMember = memberRepository.findByNickname(nickname);
         if (findMember.isPresent()) {
             // TODO 예외 클래스 만들면 수정하기
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException("이미 존재하는 별명입니다.");
         }
     }
 }
