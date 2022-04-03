ea095fab8fa17e23019a0b0d9d152712d12b98c8
JunYoung-C <doforme524@gmail.com>
2022-04-03 15:45:50 +0900

    feat: PostFile 엔티티 추가와 dto 변경 로직 controller에 배치로 인한 PostService 변경 (#16)

--- instragram/src/main/java/toyproject/instragram/service/PostService.java
+++ instragram/src/main/java/toyproject/instragram/service/PostService.java
@@ -2,19 +2,16 @@ package toyproject.instragram.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import toyproject.instragram.entity.Comment;
+import toyproject.instragram.entity.Member;
 import toyproject.instragram.entity.Post;
-import toyproject.instragram.repository.CommentRepository;
+import toyproject.instragram.entity.PostFile;
+import toyproject.instragram.repository.MemberRepository;
 import toyproject.instragram.repository.PostRepository;
-import toyproject.instragram.dto.PostCommentDto;
-import toyproject.instragram.dto.PostDto;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -22,41 +19,57 @@ import java.util.stream.Collectors;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final FileStore fileManager;
 
-    private final int MAX_COMMENT_COUNT = 3;
+    private final int MAX_POST_SIZE = 10;
 
     @Transactional
-    public Long addPost(Post post) {
-        // TODO controller 구현시 파라미터를 dto로 받고 변환하는 로직 필요
+    public Long addPost(Long memberId, List<String> filePaths, String text) {
+        Member member = memberRepository.findById(memberId).orElse(null);
+        Post post = new Post(member, text);
+        filePaths.stream().forEach(filePath -> {
+                    PostFile postFile = new PostFile(post,
+                            fileManager.extractFileName(filePath),
+                            fileManager.createStoreFileName(),
+                            fileManager.extractExtension(filePath));
+
+                    post.addPostFile(postFile);
+                }
+        );
+
         postRepository.save(post);
         return post.getId();
     }
 
-    public Slice<PostDto> getPostDtoSlice(Pageable pageable) {
-        Slice<Post> postSlice = postRepository.getPostsByOrderByCreatedDateDesc(pageable);
-        return postSlice
-                .map(post -> new PostDto(post, getCommentCount(post), getCommentDtoLists(getComments(post))));
+    public Slice<Post> getPostSlice(int page) {
+        return postRepository.getPostsByOrderByCreatedDateDesc(PageRequest.of(page, MAX_POST_SIZE));
     }
 
-    private Long getCommentCount(Post post) {
-        return commentRepository.countCommentsByPostId(post.getId());
-    }
+//    private Long getCommentCount(Post post) {
+//        return commentRepository.countCommentsByPostId(post.getId());
+//    }
 
-    private List<PostCommentDto> getCommentDtoLists(List<Comment> comments) {
-        return comments
-                .stream()
-                .map(comment -> new PostCommentDto(comment.getId(), comment.getText()))
-                .collect(Collectors.toList());
-    }
+//    private List<PostCommentDto> getCommentDtoLists(List<Comment> comments) {
+//        return comments
+//                .stream()
+//                .map(comment -> new PostCommentDto(comment.getId(), comment.getText()))
+//                .collect(Collectors.toList());
+//    }
 
-    private List<Comment> getComments(Post post) {
-        return commentRepository
-                .getCommentsByMemberIdAndPostId(
-                        post.getMember().getId(),
-                        post.getId(),
-                        PageRequest.of(0, MAX_COMMENT_COUNT))
-                .getContent();
+//    private List<Comment> getComments(Post post) {
+//        return commentRepository
+//                .getCommentsByMemberIdAndPostId(
+//                        post.getMember().getId(),
+//                        post.getId(),
+//                        PageRequest.of(0, MAX_COMMENT_COUNT))
+//                .getContent();
+//    }
+
+    @Transactional
+    public void modifyPostText(Long postId, String modifiedText) {
+        Post post = postRepository.findById(postId).orElse(null);
+        post.changeText(modifiedText);
     }
 
     @Transactional
