7001732c2224daa5dc4a392b9c68c4ed52ff86b4
JunYoung-C <doforme524@gmail.com>
2022-04-03 19:16:21 +0900

    test: MemberRepositoryTest 테스트 코드 보완

--- instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
+++ instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
@@ -16,6 +16,7 @@ import toyproject.instragram.entity.MemberImage;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,45 +30,21 @@ class MemberRepositoryTest {
     @Autowired
     EntityManager em;
 
-    @BeforeEach
-    void init() {
-
-        MemberImage memberImage1 = new MemberImage("file1", "encodedFile1", "png");
-        Member member1 = new Member(null, "junyoung", "이름1");
-        member1.addProfileImage(memberImage1);
-        memberRepository.save(member1);
-
-        MemberImage memberImage2 = new MemberImage("file2", "encodedFile2", "png");
-        Member member2 = new Member(null, "jun_young", "이름2");
-        member2.addProfileImage(memberImage2);
-        memberRepository.save(member2);
-
-        MemberImage memberImage3 = new MemberImage("file3", "encodedFile3", "png");
-        Member member3 = new Member(null, "chanyoung", "이름2");
-        member3.addProfileImage(memberImage3);
-        memberRepository.save(member3);
-
-        em.flush();
-        em.clear();
-    }
-
-    @DisplayName("닉네임으로 프로필 검색 - 성공")
+    @DisplayName("닉네임으로 프로필 검색 - 최대 50건 검색 성공")
     @Test
     public void searchByNickname() {
         //given
+        IntStream.range(0, 51).forEach(i -> {
+            Member member = new Member(null, "nickname" + i, "이름");
+            member.addProfileImage(new MemberImage("file" + i, "encodedFile" + i, "png"));
+            memberRepository.save(member);
+        });
+
         //when
-        List<MemberProfileDto> findProfiles = memberRepository.searchProfiles("jun");
+        List<MemberProfileDto> findProfiles = memberRepository.searchProfiles("ick");
 
         //then
-        assertThat(findProfiles)
-                .extracting("nickname")
-                .containsExactly("junyoung", "jun_young");
-
-        assertThat(findProfiles)
-                .extracting("imagePath")
-                .containsExactly("encodedFile1.png", "encodedFile2.png");
-
-        assertThat(findProfiles).hasSize(2);
+        assertThat(findProfiles).hasSize(50);
     }
 
     @DisplayName("닉네임으로 프로필 검색 - 결과 없음")
@@ -86,8 +63,14 @@ class MemberRepositoryTest {
     @Test
     public void findByNickname() {
         //given
+        IntStream.range(0, 3).forEach(i -> {
+            Member member = new Member(null, "nickname" + i, "이름");
+            member.addProfileImage(new MemberImage("file" + i, "encodedFile" + i, "png"));
+            memberRepository.save(member);
+        });
+
         //when
-        String nickname = "junyoung";
+        String nickname = "nickname0";
         Member findMember = memberRepository.findByNickname(nickname).orElse(null);
 
         //then
