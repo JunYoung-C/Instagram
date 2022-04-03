e34e439550a06bb2bf5b4f8be0a6c6627d09369c
JunYoung-C <doforme524@gmail.com>
2022-04-03 23:22:24 +0900

    chore: 초기 데이터 설정

--- instragram/src/main/java/toyproject/instragram/InitData.java
+++ instragram/src/main/java/toyproject/instragram/InitData.java
@@ -3,7 +3,6 @@ package toyproject.instragram;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toyproject.instragram.entity.*;
 
@@ -15,7 +14,7 @@ import java.util.stream.IntStream;
 @Profile("local")
 @Component
 @RequiredArgsConstructor
-public class InitPost {
+public class InitData {
 
     private final InitPostService initPostService;
 
@@ -32,31 +31,47 @@ public class InitPost {
         EntityManager em;
 
         public void init() {
-            Privacy privacy = Privacy.create()
-            Member member1 = new Member(null, "nickname1", "이름1");
-            Member member2 = new Member(null, "nickname2", "이름2");
+            Member member1 = new Member(
+                    Privacy.create("1234", "010-1111-1111"),
+                    "doforme",
+                    "최준영");
+            Member member2 = new Member(
+                    Privacy.create("1234", "test@naver.com"),
+                    "chlwnsdud",
+                    "김영준");
+
             em.persist(member1);
             em.persist(member2);
 
-            Post post = new Post(member1, null);
-            em.persist(post);
 
-            Comment comment = new Comment(post, member1, null);
-            em.persist(comment);
+            Post post1 = new Post(member1, "안녕하세요~");
+            post1.addPostFile(new PostFile(post1, "image", "test-image1", "png"));
+            post1.addPostFile(new PostFile(post1, "image", "test-image2", "png"));
+            post1.addPostFile(new PostFile(post1, "image", "test-image3", "png"));
+            em.persist(post1);
 
-            IntStream.range(0, 25)
-                    .forEach(i -> em.persist(new Reply(comment, i % 2 == 0 ? member1 : member2, String.valueOf(i))));
+            Post post2 = new Post(member2, "반갑습니다~");
+            post2.addPostFile(new PostFile(post2, "image", "test-image4", "png"));
+            em.persist(post2);
+
+            Comment comment1 = new Comment(post1, member1, "멋있어요!");
+            Comment comment2 = new Comment(post1, member2, "여기 어디에요?");
+            Comment comment3 = new Comment(post1, member2, "저도 갈래요!");
+            em.persist(comment1);
+            em.persist(comment2);
+            em.persist(comment3);
+
+            Reply reply1 = new Reply(comment2, member1, "여기는 북극입니다!!");
+            Reply reply2 = new Reply(comment2, member2, "저런 곳 가보는게 제 버킷리스트에요. 부러워요ㅠㅠ");
+            Reply reply3 = new Reply(comment2, member1, "다음에 갈 떄 같이 가요!!");
+            Reply reply4 = new Reply(comment2, member2, "좋아요!");
+            em.persist(reply1);
+            em.persist(reply2);
+            em.persist(reply3);
+            em.persist(reply4);
 
             em.flush();
             em.clear();
-            for (int i = 0; i < 100; i++) {
-                Member member = i % 2 == 0 ? member1 : member2;
-                Post post = new Post(member, null);
-                for (int j = 0; j < i; j++) {
-                    post.addComment(new Comment(post, i % 2 == 0 ? member1 : member2, "안녕하세요" + i + j));
-                }
-                em.persist(post);
-            }
         }
     }
 }
