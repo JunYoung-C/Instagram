029e5d8ad8406c9ce3bb847f759387da51ff83db
JunYoung-C <doforme524@gmail.com>
2022-04-04 11:38:25 +0900

    feat: MemberController 검증 로직 보완 (#20)

--- instragram/src/main/java/toyproject/instragram/controller/MemberController.java
+++ instragram/src/main/java/toyproject/instragram/controller/MemberController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import toyproject.instragram.service.MemberDto;
 import toyproject.instragram.service.MemberService;
 
+import javax.validation.Valid;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -14,11 +16,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/members")
-    public String signUp(MemberSaveForm form, BindingResult bindingResult) {
+    public String signUp(@Valid MemberSaveForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signUp";
+        }
         memberService.signUp(
                 new MemberDto(form.getPhoneNumberOrEmail(), form.getName(), form.getNickname(), form.getPassword()));
 
-        return "redirect:/signIn";
+        return "redirect:/sign-in";
     }
 
 
