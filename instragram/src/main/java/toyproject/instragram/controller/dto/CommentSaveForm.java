e2a3521559ce71bbe1e65beb1b1915e425502cc0
JunYoung-C <doforme524@gmail.com>
2022-04-05 21:42:05 +0900

    feat: Comment 관련 dto 생성

---
+++ instragram/src/main/java/toyproject/instragram/controller/dto/CommentSaveForm.java
@@ -0,0 +1,21 @@
+package toyproject.instragram.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class CommentSaveForm {
+
+    @NotNull
+    private Long postId;
+
+    @NotNull
+    private Long memberId;
+
+    @NotBlank
+    private String text;
+}
