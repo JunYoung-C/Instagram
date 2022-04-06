6315c59795628c26a1ab8d2b20501d753b275e29
JunYoung-C <doforme524@gmail.com>
2022-04-06 20:54:13 +0900

    feat: Member 엔티티에 비밀번호 검증 로직 추가

--- instragram/src/main/java/toyproject/instragram/entity/Member.java
+++ instragram/src/main/java/toyproject/instragram/entity/Member.java
@@ -22,7 +22,7 @@ public class Member extends BaseEntity {
 
     @Embedded
     private Privacy privacy;
-    private String nickname;
+    private String nickname; //TODO 회원 별명은 문자, 숫자, 밑줄 및 마침표만 사용 가능. 검증 필요
     private String name;
 
     @OneToOne(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
@@ -37,5 +37,9 @@ public class Member extends BaseEntity {
     public void addProfileImage(MemberImage memberImage) {
         this.memberImage = memberImage;
         memberImage.setMember(this);
+    }
+
+    public boolean isCorrectPassword(String password) {
+        return privacy.getPassword().equals(password);
     }
 }
