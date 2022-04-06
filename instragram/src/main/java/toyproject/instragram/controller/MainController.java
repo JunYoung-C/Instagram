72592547c44cd8f6b6818d5ab88019ed8fb76f46
JunYoung-C <doforme524@gmail.com>
2022-04-06 22:31:35 +0900

    feat: 메인 페이지에 로그인 처리 적용 (#23)

--- instragram/src/main/java/toyproject/instragram/controller/MainController.java
+++ instragram/src/main/java/toyproject/instragram/controller/MainController.java
@@ -1,13 +1,24 @@
 package toyproject.instragram.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import toyproject.instragram.SessionConst;
+import toyproject.instragram.entity.Member;
 
 @Controller
+@Slf4j
 public class MainController {
 
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(
+            @SessionAttribute(value = SessionConst.SIGN_IN_MEMBER, required = false) Member signInMember) {
+
+        if (signInMember == null) {
+            return "signIn";
+        }
+
         return "main";
     }
 }
