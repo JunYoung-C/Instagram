858e21638dc1eee07410f5c16f253d5addfd4fd1
JunYoung-C <doforme524@gmail.com>
2022-04-06 22:30:49 +0900

    feat: 로그인 인터셉터 특정 url을 제외하고 모두 적용 (#23)

--- instragram/src/main/java/toyproject/instragram/AppConfig.java
+++ instragram/src/main/java/toyproject/instragram/AppConfig.java
@@ -3,14 +3,25 @@ package toyproject.instragram;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new SignInInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/resources/**", "/error", "/", "/members/signin", "/members/signout");
+    }
+
 }
