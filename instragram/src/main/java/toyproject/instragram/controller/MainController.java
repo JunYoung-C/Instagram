e62f7ce1c52e695c4281ff0dd3716a6c67755242
JunYoung-C <doforme524@gmail.com>
2022-04-06 15:50:19 +0900

    refactor: 로그인 메소드 MemberController로 이동

--- instragram/src/main/java/toyproject/instragram/controller/MainController.java
+++ instragram/src/main/java/toyproject/instragram/controller/MainController.java
@@ -4,12 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HtmlController {
-
-    @GetMapping("/sign-in")
-    public String signInPage() {
-        return "signIn";
-    }
+public class MainController {
 
     @GetMapping("/")
     public String mainPage() {
--- instragram/src/main/java/toyproject/instragram/controller/MemberController.java
+++ instragram/src/main/java/toyproject/instragram/controller/MemberController.java
@@ -5,31 +5,53 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import toyproject.instragram.controller.dto.MemberSaveForm;
+import toyproject.instragram.controller.dto.SignInForm;
 import toyproject.instragram.service.MemberDto;
 import toyproject.instragram.service.MemberService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/members/new")
+
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "signUp";
+    }
+
+    @PostMapping("/signup")
     public String signUp(@Valid MemberSaveForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "signUp";
         }
-        memberService.signUp(
-                new MemberDto(form.getPhoneNumberOrEmail(), form.getName(), form.getNickname(), form.getPassword()));
+        memberService.signUp(new MemberDto(form.getPhoneNumberOrEmail(), form.getName(), form.getNickname(), form.getPassword()));
 
         return "redirect:/sign-in";
     }
 
-    @GetMapping("/members/new")
-    public String signUpPage() {
-        return "signUp";
+    @GetMapping("/signin")
+    public String signInPage() {
+        return "signIn";
+    }
+
+    @PostMapping("/signin")
+    public String signIn(@Valid SignInForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "signIn";
+        }
+
+        HttpSession session = request.getSession();
+//        session.setAttribute("SignInMember", );
+
+        return "redirect:/";
     }
 }
