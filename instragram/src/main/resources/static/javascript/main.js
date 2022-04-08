ab802bf89e4e6a363371b75997a2829debd9b728
JunYoung-C <doforme524@gmail.com>
2022-04-08 10:42:51 +0900

    style: 인텔리제이 자동 정렬 적용

--- instragram/src/main/resources/static/javascript/main.js
+++ instragram/src/main/resources/static/javascript/main.js
@@ -30,163 +30,163 @@ const PREVIEW_IMAGE_WIDTH = 453.59;
 const COMMENT_POST_IMAGE_WIDTH = 555;
 
 class SlideController {
-  constructor(slideWidth, imageCount, prevButton, nextButton, images) {
-    this.index = 0;
-    this.slideWidth = slideWidth;
-    this.imageCount = imageCount;
-    this.prevButton = prevButton;
-    this.nextButton = nextButton;
-    this.images = images;
+    constructor(slideWidth, imageCount, prevButton, nextButton, images) {
+        this.index = 0;
+        this.slideWidth = slideWidth;
+        this.imageCount = imageCount;
+        this.prevButton = prevButton;
+        this.nextButton = nextButton;
+        this.images = images;
 
-    this.controlButtons();
-  }
-
-  slide (selectedButton) {
-    const increment = (selectedButton === this.prevButton) ? -1 : 1;
-  
-    this.index += increment;
-    this.images.style.left = -(this.index * this.slideWidth) + "px";
-    this.controlButtons();
-  }
-  
-  controlButtons() {
-    if (this.isFirstPage()) {
-      this.hideprevButton();
-    } else {
-      this.showprevButton();
+        this.controlButtons();
     }
 
-    if (this.isLastPage()) {
-      this.hideNextButton();
-    } else {
-      this.showNextButton();
+    slide(selectedButton) {
+        const increment = (selectedButton === this.prevButton) ? -1 : 1;
+
+        this.index += increment;
+        this.images.style.left = -(this.index * this.slideWidth) + "px";
+        this.controlButtons();
     }
-  }
 
-  isFirstPage() {
-    return this.index <= 0;
-  }
+    controlButtons() {
+        if (this.isFirstPage()) {
+            this.hideprevButton();
+        } else {
+            this.showprevButton();
+        }
 
-  isLastPage() {
-    return this.index >= this.imageCount - 1;
-  }
+        if (this.isLastPage()) {
+            this.hideNextButton();
+        } else {
+            this.showNextButton();
+        }
+    }
 
-  hideprevButton() {
-    this.prevButton.style.display = "none";
-  }
+    isFirstPage() {
+        return this.index <= 0;
+    }
 
-  hideNextButton() {
-    this.nextButton.style.display = "none";
-  }
+    isLastPage() {
+        return this.index >= this.imageCount - 1;
+    }
 
-  showprevButton() {
-    this.prevButton.style.display = "flex";
-  }
+    hideprevButton() {
+        this.prevButton.style.display = "none";
+    }
 
-  showNextButton() {
-    this.nextButton.style.display = "flex";
-  }
+    hideNextButton() {
+        this.nextButton.style.display = "none";
+    }
 
-  clear(imageCount) {
-    this.index = 0;
-    this.imageCount = imageCount;
-    this.images.style.left = "0px";
-    this.controlButtons();
-  }
+    showprevButton() {
+        this.prevButton.style.display = "flex";
+    }
+
+    showNextButton() {
+        this.nextButton.style.display = "flex";
+    }
+
+    clear(imageCount) {
+        this.index = 0;
+        this.imageCount = imageCount;
+        this.images.style.left = "0px";
+        this.controlButtons();
+    }
 }
 
 function showPreviewFiles(event) {
-  const files = event.target.files;
+    const files = event.target.files;
 
-  Array.from(files).forEach((file) => {
-    const reader = new FileReader();
+    Array.from(files).forEach((file) => {
+        const reader = new FileReader();
 
-    reader.addEventListener("load", function(event) {
-      const img = document.createElement("img");
-      img.src = event.target.result;
-      img.classList.add("add-post__preview-image");
+        reader.addEventListener("load", function (event) {
+            const img = document.createElement("img");
+            img.src = event.target.result;
+            img.classList.add("add-post__preview-image");
 
-      const li = document.createElement("li");
-      li.classList.add("add-post__preview-list");
+            const li = document.createElement("li");
+            li.classList.add("add-post__preview-list");
 
-      li.appendChild(img);
-      addPostPreview.appendChild(li);
+            li.appendChild(img);
+            addPostPreview.appendChild(li);
+        });
+
+        reader.readAsDataURL(file);
     });
-  
-    reader.readAsDataURL(file);
-  });
 
-  previewSlideController.clear(files.length);
+    previewSlideController.clear(files.length);
 }
 
 function postFormClear() {
-  addPostPreview.innerHTML = "";
-  addPostFileInput.value = "";
-  addPostText.value = "";
+    addPostPreview.innerHTML = "";
+    addPostFileInput.value = "";
+    addPostText.value = "";
 }
 
 function addMainPageEvent() {
-  mainPostImagePrevButton.addEventListener("click", (event) => {
-    mainPostImageSlideController.slide(event.currentTarget);
-  });
-  
-  mainPostImageNextButton.addEventListener("click", (event) => {
-    mainPostImageSlideController.slide(event.currentTarget);
-  });
-  
-  headerSearchInput.addEventListener("focus", () => {
-    searchResultWrap.style.display = "block";
-  });
-  
-  headerSearchInput.addEventListener("blur", () => {
-    searchResultWrap.style.display = "none";
-  });
+    mainPostImagePrevButton.addEventListener("click", (event) => {
+        mainPostImageSlideController.slide(event.currentTarget);
+    });
+
+    mainPostImageNextButton.addEventListener("click", (event) => {
+        mainPostImageSlideController.slide(event.currentTarget);
+    });
+
+    headerSearchInput.addEventListener("focus", () => {
+        searchResultWrap.style.display = "block";
+    });
+
+    headerSearchInput.addEventListener("blur", () => {
+        searchResultWrap.style.display = "none";
+    });
 }
 
 function addNewPostPageEvent() {
-addPostCancleButtons.forEach((addPostCancleBtn) => {
-  addPostCancleBtn.addEventListener("click", () => {
-    addPostDiv.style.display = "none";
-    mainBody.style.overflow = "auto";
-    postFormClear();
-    previewSlideController.clear(0);
-  });
-});
+    addPostCancleButtons.forEach((addPostCancleBtn) => {
+        addPostCancleBtn.addEventListener("click", () => {
+            addPostDiv.style.display = "none";
+            mainBody.style.overflow = "auto";
+            postFormClear();
+            previewSlideController.clear(0);
+        });
+    });
 
-showAddPostButton.addEventListener("click", () => {
-  addPostDiv.style.display = "block";
-  mainBody.style.overflow = "hidden";
-});
+    showAddPostButton.addEventListener("click", () => {
+        addPostDiv.style.display = "block";
+        mainBody.style.overflow = "hidden";
+    });
 
-previewPrevButton.addEventListener("click", (event) => {
-  previewSlideController.slide(event.currentTarget);
-});
+    previewPrevButton.addEventListener("click", (event) => {
+        previewSlideController.slide(event.currentTarget);
+    });
 
-previewNextButton.addEventListener("click", (event) => {
-  previewSlideController.slide(event.currentTarget);
-});
+    previewNextButton.addEventListener("click", (event) => {
+        previewSlideController.slide(event.currentTarget);
+    });
 
-addPostFileInput.addEventListener("change", showPreviewFiles);
+    addPostFileInput.addEventListener("change", showPreviewFiles);
 }
 
 function addCommentPageEvent() {
-  commentPostImagePrevButton.addEventListener("click", (event) => {
-    commentPostSlideController.slide(event.currentTarget);
-  });
-  
-  commentPostImageNextButton.addEventListener("click", (event) => {
-    commentPostSlideController.slide(event.currentTarget);
-  });
+    commentPostImagePrevButton.addEventListener("click", (event) => {
+        commentPostSlideController.slide(event.currentTarget);
+    });
 
-  showComment.addEventListener("click", () => {
-    comment.style.display = "block";
-    mainBody.style.overflow = "hidden";
-  });
+    commentPostImageNextButton.addEventListener("click", (event) => {
+        commentPostSlideController.slide(event.currentTarget);
+    });
 
-  commentCancel.addEventListener("click", () => {
-    comment.style.display = "none";
-    mainBody.style.overflow = "auto";
-  });
+    showComment.addEventListener("click", () => {
+        comment.style.display = "block";
+        mainBody.style.overflow = "hidden";
+    });
+
+    commentCancel.addEventListener("click", () => {
+        comment.style.display = "none";
+        mainBody.style.overflow = "auto";
+    });
 }
 
 const mainPostImageSlideController = new SlideController(MAIN_POST_IMAGE_WIDTH, mainPostImagesCount, mainPostImagePrevButton, mainPostImageNextButton, mainPostImages);
