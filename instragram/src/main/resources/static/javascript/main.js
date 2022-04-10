8c2abc619aa2e987c58f7e1f76e1869f764548e2
JunYoung-C <doforme524@gmail.com>
2022-04-10 13:26:06 +0900

    feat: 더보기 버튼 클릭시 다음 게시물을 ajax로 가져오는 기능 추가 (#29)

--- instragram/src/main/resources/static/javascript/main.js
+++ instragram/src/main/resources/static/javascript/main.js
@@ -3,160 +3,165 @@ const searchResultWrap = document.querySelector(".search-result-wrap");
 const mainPostWrap = document.querySelector(".main-post-wrap");
 const mainPostTemplateHtml = document.querySelector("#template__main-post").innerHTML;
 const mainPostImagesTemplateHtml = document.querySelector("#template__main-post-images").innerHTML;
-const mainPostCommentsTemplateHtml = document.querySelector("#template__main-post-comments").innerHTML;
-
+const mainPostMore = document.querySelector(".main-post-more");
 
 const MAIN_POST_IMAGE_WIDTH = 600;
+let nextPage = 0;
 
 class SlideController {
-  constructor(slideWidth, imageCount, prevButton, nextButton, images) {
-      this.index = 0;
-      this.slideWidth = slideWidth;
-      this.imageCount = imageCount;
-      this.prevButton = prevButton;
-      this.nextButton = nextButton;
-      this.images = images;
+    constructor(slideWidth, imageCount, prevButton, nextButton, images) {
+        this.index = 0;
+        this.slideWidth = slideWidth;
+        this.imageCount = imageCount;
+        this.prevButton = prevButton;
+        this.nextButton = nextButton;
+        this.images = images;
 
-      this.controlButtons();
-  }
+        this.controlButtons();
+    }
 
-  slide(selectedButton) {
-      const increment = (selectedButton === this.prevButton) ? -1 : 1;
+    slide(selectedButton) {
+        const increment = (selectedButton === this.prevButton) ? -1 : 1;
 
-      this.index += increment;
-      this.images.style.left = -(this.index * this.slideWidth) + "px";
-      this.controlButtons();
-  }
+        this.index += increment;
+        this.images.style.left = -(this.index * this.slideWidth) + "px";
+        this.controlButtons();
+    }
 
-  controlButtons() {
-      if (this.isFirstPage()) {
-          this.hideprevButton();
-      } else {
-          this.showprevButton();
-      }
+    controlButtons() {
+        if (this.isFirstPage()) {
+            this.hideprevButton();
+        } else {
+            this.showprevButton();
+        }
 
-      if (this.isLastPage()) {
-          this.hideNextButton();
-      } else {
-          this.showNextButton();
-      }
-  }
+        if (this.isLastPage()) {
+            this.hideNextButton();
+        } else {
+            this.showNextButton();
+        }
+    }
 
-  isFirstPage() {
-      return this.index <= 0;
-  }
+    isFirstPage() {
+        return this.index <= 0;
+    }
 
-  isLastPage() {
-      return this.index >= this.imageCount - 1;
-  }
+    isLastPage() {
+        return this.index >= this.imageCount - 1;
+    }
 
-  hideprevButton() {
-      this.prevButton.style.display = "none";
-  }
+    hideprevButton() {
+        this.prevButton.style.display = "none";
+    }
 
-  hideNextButton() {
-      this.nextButton.style.display = "none";
-  }
+    hideNextButton() {
+        this.nextButton.style.display = "none";
+    }
 
-  showprevButton() {
-      this.prevButton.style.display = "flex";
-  }
+    showprevButton() {
+        this.prevButton.style.display = "flex";
+    }
 
-  showNextButton() {
-      this.nextButton.style.display = "flex";
-  }
+    showNextButton() {
+        this.nextButton.style.display = "flex";
+    }
 
-  clear(imageCount) {
-      this.index = 0;
-      this.imageCount = imageCount;
-      this.images.style.left = "0px";
-      this.controlButtons();
-  }
+    clear(imageCount) {
+        this.index = 0;
+        this.imageCount = imageCount;
+        this.images.style.left = "0px";
+        this.controlButtons();
+    }
 }
 
 function getPostsAjax() {
-  const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
-  if (!request) {
-    alert("XMLHTTP 인스턴스 생성 불가");
-    return false;
-  }
-
-  request.onreadystatechange = function() {
-    if (request.readyState === XMLHttpRequest.DONE) {
-      if (request.status === 200) {
-        console.log(request.response);
-        setMainPost(request.response);
-
-      } else {
-        alert("request에 문제가 있습니다.")
-      }
+    if (!request) {
+        alert("XMLHTTP 인스턴스 생성 불가");
+        return false;
     }
-  }
 
-  request.open("get", "/posts?page=" + 0);
-  request.responseType = "json";
-  request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                console.log(request.response);
+                setMainPost(request.response);
+            } else {
+                alert("request에 문제가 있습니다.")
+            }
+        }
+    }
+
+    request.open("get", "/posts?page=" + nextPage);
+    request.responseType = "json";
+    request.send();
 }
 
 function setMainPost(response) {
-  const posts = response.data;
-  const sliceInfo = response.sliceInfo;
+    const posts = response.data;
+    const sliceInfo = response.sliceInfo;
 
-  for (let i = 0; i < posts.length; i++) {
-    mainPostWrap.innerHTML += mainPostTemplateHtml
-      .replace("{postId}", posts[i].postId)
-      .replace("{postId}", posts[i].postId)
-      .replace("{member.memberId}", posts[i].member.memberId)
-      .replace("{member.nickname}", posts[i].member.nickname)
-      .replace("{member.nickname}", posts[i].member.nickname)
-      .replace("{member.imagePath}", posts[i].member.imagePath)
-      .replace("{text}", posts[i].text)
-      .replace("{commentCount}", posts[i].commentCount)
-      .replace("{createdDate}", posts[i].createdDate);
+    for (let i = 0; i < posts.length; i++) {
+        mainPostWrap.innerHTML += mainPostTemplateHtml
+            .replace("{postId}", posts[i].postId)
+            .replace("{postId}", posts[i].postId)
+            .replace("{member.memberId}", posts[i].member.memberId)
+            .replace("{member.nickname}", posts[i].member.nickname)
+            .replace("{member.nickname}", posts[i].member.nickname)
+            .replace("{member.imagePath}", posts[i].member.imagePath)
+            .replace("{text}", posts[i].text)
+            .replace("{commentCount}", posts[i].commentCount)
+            .replace("{createdDate}", posts[i].createdDate);
 
-      const mainPostImages = document.querySelector(`.main-post-${posts[i].postId} .main-post-images`);
-      const ownerCommentsWrap = document.querySelector(`.main-post-${posts[i].postId} .post-comments__owner-comments-wrap`);
-      for (let j = 0; j < posts[i].filePaths.length; j++) {
-        mainPostImages.innerHTML += mainPostImagesTemplateHtml.replace("{filePath}", posts[i].filePaths[j]);
-      }
-      // 더보기 버튼
+        const mainPostImages = document.querySelector(`.main-post-${posts[i].postId} .main-post-images`);
+        const ownerCommentsWrap = document.querySelector(`.main-post-${posts[i].postId} .post-comments__owner-comments-wrap`);
+        for (let j = 0; j < posts[i].filePaths.length; j++) {
+            mainPostImages.innerHTML += mainPostImagesTemplateHtml.replace("{filePath}", posts[i].filePaths[j]);
+        }
+        
+        nextPage = sliceInfo.page + 1;
+        if (!sliceInfo.hasNext) {
+          mainPostMore.style.display = "none";
+          mainPostMore.removeEventListener("click", getPostsAjax);
+        }
 
-  }
-  
-  for (let i = 0; i < posts.length; i++) {
-    const mainPostImagesCount = posts[i].filePaths.length;
-    const mainPostImagePrevButton = document.querySelector(`.main-post-${posts[i].postId} .post-images__prev-button`);
-    const mainPostImageNextButton = document.querySelector(`.main-post-${posts[i].postId} .post-images__next-button`);
-    const mainPostImages = document.querySelector(`.main-post-${posts[i].postId} .main-post-images`);
-    const mainPostImageSlideController = new SlideController(MAIN_POST_IMAGE_WIDTH, mainPostImagesCount, mainPostImagePrevButton, mainPostImageNextButton, mainPostImages);
-    const showComment = document.querySelector(".show-comment"); // ajax로 가져와서 모달창 띄우도록 변경
+    }
 
-    mainPostImagePrevButton.addEventListener("click", (event) => {
-      mainPostImageSlideController.slide(event.currentTarget);
-    });
+    for (let i = 0; i < posts.length; i++) {
+        const mainPostImagesCount = posts[i].filePaths.length;
+        const mainPostImagePrevButton = document.querySelector(`.main-post-${posts[i].postId} .post-images__prev-button`);
+        const mainPostImageNextButton = document.querySelector(`.main-post-${posts[i].postId} .post-images__next-button`);
+        const mainPostImages = document.querySelector(`.main-post-${posts[i].postId} .main-post-images`);
+        const mainPostImageSlideController = new SlideController(MAIN_POST_IMAGE_WIDTH, mainPostImagesCount, mainPostImagePrevButton, mainPostImageNextButton, mainPostImages);
+        const showComment = document.querySelector(".show-comment"); // ajax로 가져와서 모달창 띄우도록 변경
 
-    mainPostImageNextButton.addEventListener("click", (event) => {
-      mainPostImageSlideController.slide(event.currentTarget);
-    });
+        mainPostImagePrevButton.addEventListener("click", (event) => {
+            mainPostImageSlideController.slide(event.currentTarget);
+        });
 
-    showComment.addEventListener("click", () => {
-      comment.style.display = "block";
-      mainBody.style.overflow = "hidden";
-    });
-  }
+        mainPostImageNextButton.addEventListener("click", (event) => {
+          console.log(1);
+            mainPostImageSlideController.slide(event.currentTarget);
+        });
+
+        showComment.addEventListener("click", () => {
+            comment.style.display = "block";
+            mainBody.style.overflow = "hidden";
+        });
+    }
 }
 
 function addMainPageEvent() {
-    
+    mainPostMore.addEventListener("click", getPostsAjax);
 
-  headerSearchInput.addEventListener("focus", () => {
-      searchResultWrap.style.display = "block";
-  });
+    headerSearchInput.addEventListener("focus", () => {
+        searchResultWrap.style.display = "block";
+    });
 
-  headerSearchInput.addEventListener("blur", () => {
-      searchResultWrap.style.display = "none";
-  });
+    headerSearchInput.addEventListener("blur", () => {
+        searchResultWrap.style.display = "none";
+    });
 }
 
 
