97ae15d8490faf58c9d45aeb27ebb770dc7c51f9
JunYoung-C <doforme524@gmail.com>
2022-04-16 20:18:58 +0900

    fix: 잘못된 인자 위치 변경

--- instragram/src/main/java/toyproject/instragram/common/exception/form/FormExceptionType.java
+++ instragram/src/main/java/toyproject/instragram/common/exception/form/FormExceptionType.java
@@ -14,33 +14,35 @@ import toyproject.instragram.common.exception.form.signup.*;
 public enum FormExceptionType {
     //로그인 관련 에러
     NOT_FOUND_ACCOUNT_BY_NICKNAME(new NotFoundAccountByNicknameException(
-            "signInId", "incorrect",
-            "입력한 사용자 이름을 사용하는 계정을 찾을 수 없습니다. 사용자 이름을 확인하고 다시 시도하세요.")),
+            "입력한 사용자 이름을 사용하는 계정을 찾을 수 없습니다. 사용자 이름을 확인하고 다시 시도하세요.",
+            "signInId", "incorrect")),
     NOT_FOUND_ACCOUNT_BY_Email(new NotFoundAccountByEmailException(
-            "signInId", "incorrect",
-            "입력한 이메일을 사용하는 계정을 찾을 수 없습니다. 사용자의 이메일을 확인하고 다시 시도하세요.")),
+            "입력한 이메일을 사용하는 계정을 찾을 수 없습니다. 사용자의 이메일을 확인하고 다시 시도하세요.",
+            "signInId", "incorrect")),
     NOT_FOUND_ACCOUNT_BY_Phone_Number(new NotFoundAccountByPhoneNumberException(
-            "password", "incorrect",
-            "입력한 휴대폰 번호를 사용하는 계정을 찾을 수 없습니다. 사용자의 휴대폰 번호를 확인하고 다시 시도하세요.")),
+            "입력한 휴대폰 번호를 사용하는 계정을 찾을 수 없습니다. 사용자의 휴대폰 번호를 확인하고 다시 시도하세요.",
+            "password", "incorrect")),
     INCORRECT_PASSWORD(new IncorrectPasswordException(
-            "password", "incorrect", "잘못된 비밀번호입니다. 다시 확인하세요.")),
+            "잘못된 비밀번호입니다. 다시 확인하세요.", "password", "incorrect")),
 
     // 회원가입 관련 에러
     DUPLICATE_NICKNAME(new DuplicateNicknameException(
-            "nickname", "duplicate", "다른 계정에서 이미 사용중인 사용자 이름입니다.")),
+            "다른 계정에서 이미 사용중인 사용자 이름입니다.", "nickname", "duplicate")),
     DUPLICATE_EMAIL(new DuplicateEmailException(
-            "phoneNumberOrEmail", "duplicate", "다른 계정에서 이미 사용중인 이메일입니다.")),
+            "다른 계정에서 이미 사용중인 이메일입니다.", "phoneNumberOrEmail", "duplicate")),
     DUPLICATE_PHONE_NUMBER(new DuplicateEmailException(
-            "phoneNumberOrEmail", "duplicate", "다른 계정에서 이미 사용중인 휴대폰 번호입니다.")),
+            "다른 계정에서 이미 사용중인 휴대폰 번호입니다.", "phoneNumberOrEmail", "duplicate")),
     INVALID_NICKNAME_BY_NUMBER(new InvalidNicknameByNumberException(
-            "nickname", "invalid", "사용자의 이름에 숫자만 포함할 수는 없습니다.")),
+            "사용자의 이름에 숫자만 포함할 수는 없습니다.", "nickname", "invalid")),
     INVALID_NICKNAME_BY_STRING(new InvalidNicknameByStringException(
-            "nickname", "invalid", "사용자 이름에는 문자, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.")),
+            "사용자 이름에는 문자, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.", "nickname", "invalid")),
     INVALID_EMAIL(new InvalidEmailException(
-            "phoneNumberOrEmail", "invalid", "유효한 이메일 주소를 입력하세요.")),
+            "유효한 이메일 주소를 입력하세요.", "phoneNumberOrEmail", "invalid")),
     INVALID_PHONE_NUMBER(new InvalidPhoneNumberException(
-            "phoneNumberOrEmail", "invalid", "유효한 휴대폰 번호를 입력하세요."));
+            "유효한 휴대폰 번호를 입력하세요.", "phoneNumberOrEmail", "invalid")),
 
+    // 파일 관련
+    EMPTY_FILE(new EmptyFileException("파일을 하나 이상 첨부하세요.", "files", "required"));
 
     private final CustomException exception;
 }
