adc1e01a02b0a2ece341412b21cafc15213181c9
JunYoung-C <doforme524@gmail.com>
2022-04-18 14:18:18 +0900

    feat: advice에 로깅 추가

--- instragram/src/main/java/toyproject/instragram/common/exception/api/ExceptionApiControllerAdvice.java
+++ instragram/src/main/java/toyproject/instragram/common/exception/api/ExceptionApiControllerAdvice.java
@@ -1,14 +1,17 @@
 package toyproject.instragram.common.exception.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionApiControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleCustomApiException(CustomApiException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(e.getStatus())
                 .body(new ExceptionResponse(e.getMessage()));
     }
