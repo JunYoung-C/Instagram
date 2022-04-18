3880ac3b8fb2e49fe588fddb144d5d931afde04e
JunYoung-C <doforme524@gmail.com>
2022-04-18 09:59:24 +0900

    feat: 댓글이 존재하는지 확인하는 메소드 추가

--- instragram/src/main/java/toyproject/instragram/comment/repository/CommentRepository.java
+++ instragram/src/main/java/toyproject/instragram/comment/repository/CommentRepository.java
@@ -12,4 +12,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph(attributePaths = {"member"})
     Slice<Comment> getCommentsByPostIdOrderByCreatedDateDesc(Long postId, Pageable pageable);
+
+    boolean existsById(Long commentId);
 }
