5b15c341fd790ce0b4bdc3d637c17b402aacd2b7
JunYoung-C <doforme524@gmail.com>
2022-04-19 15:53:32 +0900

    test: MemberRepository 테스트 코드 보완 (#43)

--- instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
+++ instragram/src/test/java/toyproject/instragram/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package toyproject.instragram.repository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,7 +10,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import toyproject.instragram.common.AppConfig;
 import toyproject.instragram.member.entity.Member;
-import toyproject.instragram.member.entity.MemberImage;
 import toyproject.instragram.member.entity.Privacy;
 import toyproject.instragram.member.repository.MemberProfileDto;
 import toyproject.instragram.member.repository.MemberRepository;
@@ -44,65 +44,124 @@ class MemberRepositoryTest {
         });
     }
 
+    @Nested
     @DisplayName("닉네임으로 프로필 검색")
-    @Test
-    public void searchByNickname() {
-        //given
-        //when
-        int pageSize = 50;
-        List<MemberProfileDto> findProfiles =
-                memberRepository.searchProfiles("ick", PageRequest.of(0, pageSize));
+    class searchByNicknameTest {
 
-        //then
-        assertThat(findProfiles).hasSize(pageSize);
+        @DisplayName("성공")
+        @Test
+        public void success() {
+            //given
+            //when
+            int pageSize = 50;
+            List<MemberProfileDto> findProfiles =
+                    memberRepository.searchProfiles("ick", PageRequest.of(0, pageSize));
+
+            //then
+            assertThat(findProfiles).hasSize(pageSize);
+        }
+
+        @DisplayName("결과 없음")
+        @Test
+        public void isEmpty() {
+            //given
+            //when
+            int pageSize = 50;
+            List<MemberProfileDto> findProfiles =
+                    memberRepository.searchProfiles(" ", PageRequest.of(0, pageSize));
+
+            //then
+            assertThat(findProfiles.size()).isEqualTo(0);
+        }
     }
 
-    @DisplayName("닉네임으로 프로필 검색 - 결과 없음")
-    @Test
-    public void searchByNickname_fail() {
-        //given
-        //when
-        int pageSize = 50;
-        List<MemberProfileDto> findProfiles =
-                memberRepository.searchProfiles(" ", PageRequest.of(0, pageSize));
-
-        //then
-        assertThat(findProfiles.size()).isEqualTo(0);
-    }
-
+    @Nested
     @DisplayName("핸드폰 번호가 정확히 일치하는 회원 조회")
-    @Test
-    public void findByPrivacyPhoneNumber() {
-        //given
-        //when
-        String phoneNumber = "01012345621";
-        Member findMember = memberRepository.findByPrivacyPhoneNumber(phoneNumber).orElse(null);
+    class findByPrivacyPhoneNumberTest {
 
-        //then
-        assertThat(findMember.getPrivacy().getPhoneNumber()).isEqualTo(phoneNumber);
+        @DisplayName("성공")
+        @Test
+        public void success() {
+            //given
+            //when
+            String phoneNumber = "01012345621";
+            Member findMember = memberRepository.findByPrivacyPhoneNumber(phoneNumber).orElse(null);
+
+            //then
+            assertThat(findMember.getPrivacy().getPhoneNumber()).isEqualTo(phoneNumber);
+        }
+
+
+        @DisplayName("결과 없음")
+        @Test
+        public void isNull() {
+            //given
+            //when
+            String phoneNumber = "010123456";
+            Member findMember = memberRepository.findByPrivacyPhoneNumber(phoneNumber).orElse(null);
+
+            //then
+            assertThat(findMember).isNull();
+        }
     }
 
+
+    @Nested
     @DisplayName("이메일이 정확히 일치하는 회원 조회")
-    @Test
-    public void findByPrivacyEmail() {
-        //given
-        //when
-        String email = "0@naver.com";
-        Member findMember = memberRepository.findByPrivacyEmail(email).orElse(null);
+    class findByPrivacyEmailTest {
 
-        //then
-        assertThat(findMember.getPrivacy().getEmail()).isEqualTo(email);
+        @DisplayName("성공")
+        @Test
+        public void success() {
+            //given
+            //when
+            String email = "0@naver.com";
+            Member findMember = memberRepository.findByPrivacyEmail(email).orElse(null);
+
+            //then
+            assertThat(findMember.getPrivacy().getEmail()).isEqualTo(email);
+        }
+
+        @DisplayName("결과 없음")
+        @Test
+        public void isNull() {
+            //given
+            //when
+            String email = "-1@naver.com";
+            Member findMember = memberRepository.findByPrivacyEmail(email).orElse(null);
+
+            //then
+            assertThat(findMember).isNull();
+        }
     }
 
-    @DisplayName("닉네임이 정확히 일치하는 회원 조회")
-    @Test
-    public void findByNickname() {
-        //given
-        //when
-        String nickname = "nickname0";
-        Member findMember = memberRepository.findByNickname(nickname).orElse(null);
 
-        //then
-        assertThat(findMember.getNickname()).isEqualTo(nickname);
+    @Nested
+    @DisplayName("닉네임이 정확히 일치하는 회원 조회")
+    class findByNicknameTest {
+
+        @DisplayName("성공")
+        @Test
+        public void success() {
+            //given
+            //when
+            String nickname = "nickname0";
+            Member findMember = memberRepository.findByNickname(nickname).orElse(null);
+
+            //then
+            assertThat(findMember.getNickname()).isEqualTo(nickname);
+        }
+
+        @DisplayName("결과 없음")
+        @Test
+        public void isNull() {
+            //given
+            //when
+            String nickname = "nickname-1";
+            Member findMember = memberRepository.findByNickname(nickname).orElse(null);
+
+            //then
+            assertThat(findMember).isNull();
+        }
     }
 }
