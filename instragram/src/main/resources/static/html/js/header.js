f0108d2f36b9018ae6f0697f3dacf1c19fa41d68
JunYoung-C <doforme524@gmail.com>
2022-06-01 17:56:54 +0900

    feat: 헤더 검색창, 네비창이 더 편리하게 사라지도록 변경 (#48)

--- instragram/src/main/resources/static/html/js/header.js
+++ instragram/src/main/resources/static/html/js/header.js
@@ -1,21 +1,34 @@
 const headerSearchResult = document.querySelector(".header-search__result");
 const container = document.querySelector("#container");
 const navOptions = document.querySelector(".nav__options");
+const headerSearchInput = document.querySelector(".header-search__input");
 
-document.querySelector(".header-search__input").addEventListener("focus", () => {
+headerSearchInput.addEventListener("focus", () => {
   headerSearchResult.style.display = "block";
 });
 
-container.addEventListener("click", () => {
-  headerSearchResult.style.display = "none";
+document.querySelector("body").addEventListener("click", (event) => {
+  if (event.target.closest(".header-search__input")) {
+    return;
+  }
+
+  if (!event.target.closest(".search-result")) {
+    headerSearchResult.style.display = "none";
+  }
 });
 
 document.querySelector(".nav--account").addEventListener("click", () => {
   navOptions.style.display = "block";
 });
 
-container.addEventListener("click", () => {
-  navOptions.style.display = "none";
+document.querySelector("body").addEventListener("click", (event) => {
+  if (event.target.closest(".nav--account")) {
+    return;
+  }
+
+  if (!event.target.closest(".nav-options")) {
+    navOptions.style.display = "none";
+  }
 });
 
 
