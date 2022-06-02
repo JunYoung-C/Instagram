161d883336bd552a25b7c15803c422cca5d6c6e9
JunYoung-C <doforme524@gmail.com>
2022-06-02 13:03:15 +0900

    feat: 게시물/댓글 좋아요, 댓글 아이콘/답글 달기 클릭 시 input 이벤트 기능 구현 (#48)

--- instragram/src/main/resources/static/html/js/post.js
+++ instragram/src/main/resources/static/html/js/post.js
@@ -41,9 +41,70 @@ function createMoreSpan(text) {
   return span;
 }
 
+function addCommentInputEvent() {
+  const commentForm = document.querySelector(".post__comment-form");
+  const commentFormText = document.querySelector(".comment-form__text");
+
+  document.querySelector(".post__comments").addEventListener("click", (event) => {
+    if (event.target.classList.contains("comment_owner__reply")) {
+      commentFormText.placeholder = "답글 달기...";
+      commentForm.setAttribute("action", "/replies");
+      // document.querySelector(".post-comment__comment-id-input").value = event.target.getAttribute("commentId");
+      commentFormText.focus();
+    }
+  });
+  
+  document.querySelector(".post-option__comment").addEventListener("click", function() {
+    commentFormText.placeholder = "댓글 달기...";
+    commentForm.setAttribute("action", "/comments")
+    commentFormText.focus();
+  })
+}
+
+function addLikePostEvent() {
+  const postOptionHeart = document.querySelector(".post-option__heart");
+  const postOptionCompactedHeart = document.querySelector(".post-option__heart--compacted");
+  
+  postOptionHeart.addEventListener("click", function() {
+    postOptionCompactedHeart.classList.remove("post-option__heart--unvisible");
+    postOptionHeart.classList.add("post-option__heart--unvisible");
+  });
+  
+  postOptionCompactedHeart.addEventListener("click", function() {
+    postOptionHeart.classList.remove("post-option__heart--unvisible");
+    postOptionCompactedHeart.classList.add("post-option__heart--unvisible");
+  });
+}
+
+function addLikeCommentEvent() {
+  const UNVISIBLE_CLASS = "comment-owner__heart--unvisible";
+
+  document.querySelector(".post__comments").addEventListener("click", function(event) {
+    const target = event.target;
+    console.log(target);
+    if (target.classList.contains("comment-owner__heart")) {
+      target.classList.add(UNVISIBLE_CLASS);
+      target.parentNode.querySelector(".comment-owner__heart--compacted").classList.remove(UNVISIBLE_CLASS);
+    }
+  });
+
+  document.querySelector(".post__comments").addEventListener("click", function(event) {
+    const target = event.target;
+    console.log(target);
+    if (target.classList.contains("comment-owner__heart--compacted")) {
+      target.classList.add(UNVISIBLE_CLASS);
+      target.parentNode.querySelector(".comment-owner__heart").classList.remove(UNVISIBLE_CLASS);
+    }
+  });
+}
+
+
 function addPostPageEvent() {
   addPostSlidingEvent();
   reduceDescription();
+  addCommentInputEvent();
+  addLikePostEvent();
+  addLikeCommentEvent();
 }
 
 addPostPageEvent();
