d0edb61fd4139256d4718c0ab08ef00b692e53a9
JunYoung-C <doforme524@gmail.com>
2022-06-03 19:27:19 +0900

    feat: 메인 페이지 게시글 축약, 좋아요 기능 추가 (#48)

--- instragram/src/main/resources/static/html/js/main.js
+++ instragram/src/main/resources/static/html/js/main.js
@@ -1,5 +1,6 @@
 const postArr = document.querySelectorAll(".post");
 
+
 function addPostSlidingEvent(post) {
   const MAIN_POST_IMAGE_WIDTH = 600;
   const leftSlideButton = post.querySelector(".slide-button--left");
@@ -28,10 +29,48 @@ function addPostFilterEvent() {
   });
 }
 
+function reduceDescription(post) {
+  const postContentDescription = post.querySelector(".post-content__description");
+  const text = postContentDescription.innerText;
+  if (text.length < 300) {
+    return;
+  }
+
+  const shortText = text.substring(0, 100) + "...";
+  
+  postContentDescription.innerText = shortText;
+  postContentDescription.appendChild(createMoreSpan(text, postContentDescription));
+}
+
+function createMoreSpan(text, postDescription) {
+  const span = document.createElement("span");
+  span.classList.add("post-content__more-description");
+  span.innerText = "더 보기";
+
+  span.addEventListener("click", function() {
+    postDescription.innerText = text;
+  });
+
+  return span;
+}
+
+function addLikePostEvent(post) {
+  const postLikeOption = post.querySelector(".post-option--like");
+
+  postLikeOption.addEventListener("click", function() {
+    post.querySelector(".post-content__like-icon").classList.toggle("post-content__like-icon--unvisible");
+    post.querySelector(".post-content__like-icon--compacted").classList.toggle("post-content__like-icon--unvisible");
+  });
+}
+
 // 좋아요 ajax
 
 function addMainPageEvent() {
-  postArr.forEach(post => addPostSlidingEvent(post));
+  postArr.forEach(post => {
+    addPostSlidingEvent(post);
+    reduceDescription(post);
+    addLikePostEvent(post);
+  });
   addPostFilterEvent();
 }
 
